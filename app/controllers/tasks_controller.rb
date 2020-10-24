28604d5f3d09d82caba7c1ff2b0ad7c74fafe709
taro.kirameki <taro.kirameki@techacademy.jp>
2020-10-24 08:21:29 +0000

    fix1024

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,10 @@
 class TasksController < ApplicationController
     before_action :require_user_logged_in
+    before_action :correct_user, only: [:destroy]
     
     def index
-        @tasks = Task.all
+        @tasks = current_user.tasks.order(id: :desc)
+
     end
     
     def show
@@ -14,7 +16,7 @@ class TasksController < ApplicationController
     end
     
     def create
-        @task = Task.new(task_params)
+        @task = current_user.tasks.build(task_params)
         
         if @task.save
             flash[:success] = 'タスクが追加されました'
@@ -30,6 +32,7 @@ class TasksController < ApplicationController
         
         if @task.update(task_params)
             flash[:success] = 'タスクが更新されました！'
+            
             redirect_to @task
         else
             flash.now[:danger] = 'エラーです'
@@ -43,15 +46,21 @@ class TasksController < ApplicationController
     end
     
     def destroy
-        @task = Task.find(params[:id])
         @task.destroy
         
         flash[:success] = 'タスクを削除しました！'
-        redirect_to tasks_url
+        redirect_to root_url
     end
     
     private
     def task_params
         params.require(:task).permit(:status, :content)
+    end
+    
+    def correct_user
+        @task = current_user.tasks.find_by(id: params[:id])
+        unless @task
+        redirect_to root_url    
+        end
     end
 end
